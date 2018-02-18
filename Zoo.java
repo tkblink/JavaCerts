52d6ed49282b329ceaba39a9e1009ac5da85e274
tkblink <tkblink@gmail.com>
2018-02-18 04:12:40 -0500

    Pass two arguments to main method. Will print Arguments

---
+++ Zoo.java
@@ -0,0 +1,10 @@
+public class Zoo {
+  public static void main(String[] args){
+    //This will require two arguments that will
+    //be printed. EX: Java Zoo "San Diego" Zoo
+    //will print two lines, San Diego and Zoo.
+    //The quotes create a new line after.
+    System.out.println(args[0]);
+    System.out.println(args[1]);
+  }
+}
