66f849e2277f5a3a975a74a164a703f58cf565ed
tkblink <tkblink@gmail.com>
2018-02-18 04:15:29 -0500

    Prints a random number between 0 and 9

---
+++ ImportRandom.java
@@ -0,0 +1,8 @@
+//Import Random Class
+import java.util.Random;
+public class ImportRandom {
+	public static void main(String[] args){
+		Random r = new Random();
+		System.out.println(r.nextInt(10)); //prints a random number between 0 and 9.
+		}
+}
